5f36457a26f4d7951f4d63e15b014b52884a2d46
K0tlyarski <djgame2.9.6@gmail.com>
2022-11-27 23:47:42 +0400

    Initial Commit

---
+++ test1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  test1
+//
+//  Created by Вадим Котлярский on 24.09.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
